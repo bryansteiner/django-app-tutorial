ae355738d566011e8c726d6ae36195bed8e65c3b
Bryan Steiner <steiner.bs@gmail.com>
2020-06-17 19:07:54 -0400

    Part 7 complete

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,4 +1,32 @@
 from django.contrib import admin
-from .models import Question
 
-admin.site.register(Question)
+from .models import Question, Choice
+
+
+# class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    # reorder admin console fields
+    # fields = ['pub_date', 'question_text']
+
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+    list_per_page = 100     # default
+
+
+# admin.site.register(Choice)
+# ^ allows us to create a choice in a choice creation form
+#   but not w/in a question creation form (foreign key)
+admin.site.register(Question, QuestionAdmin)
+
+
